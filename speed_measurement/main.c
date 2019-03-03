d67c92498ab8865113b23e45d5035ac1a21c4d41
Hiroki Yamaguchi <hiroki6357@gmail.com>
2019-03-03 21:54:33 +0900

    add speed measurement

---
+++ speed_measurement/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/times.h>
+#include <limits.h>
+
+#define BUFFER_SIZE 256
+
+int main() {
+    struct tms tms0, tms1;
+    clock_t clk0, clk1;
+    double hz = sysconf(_SC_CLK_TCK);
+
+    clk0 = times(&tms0);
+
+    // measurement task
+    printf("uint %u\n",  UINT_MAX);
+    for(unsigned int i=0; i<1000000;) {
+    //for(unsigned int i=0; i<=4000000000;) {
+        printf("num %u\n",  i);
+        i++;
+    }
+
+
+    clk1 = times(&tms1);
+
+    // print result
+    printf("clock %.3fhz\n",  hz);
+    printf("real  %.3f\n", (clk1 - clk0) / hz);
+    printf("user  %.3f\n", (tms1.tms_utime - tms0.tms_utime) / hz);
+    printf("sys   %.3f\n", (tms1.tms_stime - tms0.tms_stime) / hz);
+
+    return 0;
+}
+
